48979445992a0c0c4bb1519be87cf9fd45389470
xulihang <xulihanghai@163.com>
2022-11-14 19:20:58 -0800

    iOS: implement initialize, initLicense and initRuntimeSettingsWithString

--- ios/Plugin/DocumentNormalizerPlugin.swift
+++ ios/Plugin/DocumentNormalizerPlugin.swift
@@ -1,16 +1,45 @@
 import Foundation
 import Capacitor
+import DynamsoftDocumentNormalizer
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
  * here: https://capacitorjs.com/docs/plugins/ios
  */
 @objc(DocumentNormalizerPlugin)
-public class DocumentNormalizerPlugin: CAPPlugin {
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.resolve([
-            "value": implementation.echo(value)
-        ])
+public class DocumentNormalizerPlugin: CAPPlugin,LicenseVerificationListener   {
+    private var ddn:DynamsoftDocumentNormalizer!;
+    @objc func initialize(_ call: CAPPluginCall) {
+        ddn = DynamsoftDocumentNormalizer()
+        call.resolve()
+    }
+    
+    @objc func initLicense(_ call: CAPPluginCall) {
+        let license = call.getString("license") ?? "DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ=="
+        DynamsoftLicenseManager.initLicense(license, verificationDelegate: self)
+        call.resolve()
+    }
+    
+    @objc func initRuntimeSettingsFromString(_ call: CAPPluginCall) {
+        let template = call.getString("template") ?? ""
+        if ddn != nil {
+            if template != "" {
+                do {
+                    try ddn.initRuntimeSettingsFromString(template)
+                    call.resolve()
+                }catch {
+                    print("Unexpected error: \(error).")
+                    call.reject(error.localizedDescription)
+                }
+            }else{
+                call.reject("Empty template")
+            }
+        }else{
+            call.reject("DDN not initialized")
+        }
+    }
+    
+    public func licenseVerificationCallback(_ isSuccess: Bool, error: Error?) {
+        print(isSuccess)
     }
 }
