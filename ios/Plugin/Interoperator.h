4b6f299a161eb0a1146a6a00b8453bd139936d8f
xulihang <xulihanghai@163.com>
2023-12-08 17:21:11 +0800

    update example for mac

---
+++ ios/Plugin/Interoperator.h
@@ -0,0 +1,13 @@
+//
+//  Interoperator.h
+//  Plugin
+//
+//  Created by 徐力航 on 2023/12/8.
+//  Copyright © 2023 Max Lynch. All rights reserved.
+//
+
+#ifndef Interoperator_h
+#define Interoperator_h
+
+
+#endif /* Interoperator_h */
---
+++ ios/Plugin/Interoperator.m
@@ -0,0 +1,9 @@
+//
+//  Interoperator.m
+//  Plugin
+//
+//  Created by 徐力航 on 2023/12/8.
+//  Copyright © 2023 Max Lynch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
