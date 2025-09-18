efb9af10c9fac1d8c51b392bf58c53d92448b52e
xulihang <xulihanghai@163.com>
2025-09-18 14:47:07 +0800

    update DCV android to 3.0

--- android/src/main/java/com/dynamsoft/capacitor/ddn/DocumentNormalizerPlugin.java
+++ android/src/main/java/com/dynamsoft/capacitor/ddn/DocumentNormalizerPlugin.java
@@ -4,16 +4,16 @@ import android.graphics.Bitmap;
 import android.graphics.Point;
 import android.util.Log;
 
-import com.dynamsoft.core.basic_structures.CapturedResult;
 import com.dynamsoft.core.basic_structures.CapturedResultItem;
 import com.dynamsoft.core.basic_structures.Quadrilateral;
 import com.dynamsoft.cvr.CaptureVisionRouter;
 import com.dynamsoft.cvr.CaptureVisionRouterException;
+import com.dynamsoft.cvr.CapturedResult;
 import com.dynamsoft.cvr.SimplifiedCaptureVisionSettings;
+import com.dynamsoft.ddn.DeskewedImageResultItem;
 import com.dynamsoft.ddn.DetectedQuadResultItem;
-import com.dynamsoft.ddn.NormalizedImageResultItem;
 import com.dynamsoft.license.LicenseManager;
-import com.dynamsoft.utility.ImageManager;
+import com.dynamsoft.utility.ImageIO;
 import com.getcapacitor.JSArray;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
@@ -35,12 +35,7 @@ public class DocumentNormalizerPlugin extends Plugin {
     @PluginMethod
     public void initialize(PluginCall call) {
         if (cvr == null) {
-            cvr = new CaptureVisionRouter(getContext());
-            try {
-                cvr.initSettings("{\"CaptureVisionTemplates\": [{\"Name\": \"Default\"},{\"Name\": \"DetectDocumentBoundaries_Default\",\"ImageROIProcessingNameArray\": [\"roi-detect-document-boundaries\"]},{\"Name\": \"DetectAndNormalizeDocument_Binary\",\"ImageROIProcessingNameArray\": [\"roi-detect-and-normalize-document-binary\"]},{\"Name\": \"DetectAndNormalizeDocument_Gray\",\"ImageROIProcessingNameArray\": [\"roi-detect-and-normalize-document-gray\"]},{\"Name\": \"DetectAndNormalizeDocument_Color\",\"ImageROIProcessingNameArray\": [\"roi-detect-and-normalize-document-color\"]},{\"Name\": \"NormalizeDocument_Binary\",\"ImageROIProcessingNameArray\": [\"roi-normalize-document-binary\"]},{\"Name\": \"NormalizeDocument_Gray\",\"ImageROIProcessingNameArray\": [\"roi-normalize-document-gray\"]},{\"Name\": \"NormalizeDocument_Color\",\"ImageROIProcessingNameArray\": [\"roi-normalize-document-color\"]}],\"TargetROIDefOptions\": [{\"Name\": \"roi-detect-document-boundaries\",\"TaskSettingNameArray\": [\"task-detect-document-boundaries\"]},{\"Name\": \"roi-detect-and-normalize-document-binary\",\"TaskSettingNameArray\": [\"task-detect-and-normalize-document-binary\"]},{\"Name\": \"roi-detect-and-normalize-document-gray\",\"TaskSettingNameArray\": [\"task-detect-and-normalize-document-gray\"]},{\"Name\": \"roi-detect-and-normalize-document-color\",\"TaskSettingNameArray\": [\"task-detect-and-normalize-document-color\"]},{\"Name\": \"roi-normalize-document-binary\",\"TaskSettingNameArray\": [\"task-normalize-document-binary\"]},{\"Name\": \"roi-normalize-document-gray\",\"TaskSettingNameArray\": [\"task-normalize-document-gray\"]},{\"Name\": \"roi-normalize-document-color\",\"TaskSettingNameArray\": [\"task-normalize-document-color\"]}],\"DocumentNormalizerTaskSettingOptions\": [{\"Name\": \"task-detect-and-normalize-document-binary\",\"ColourMode\": \"ICM_BINARY\",\"SectionImageParameterArray\": [{\"Section\": \"ST_REGION_PREDETECTION\",\"ImageParameterName\": \"ip-detect-and-normalize\"},{\"Section\": \"ST_DOCUMENT_DETECTION\",\"ImageParameterName\": \"ip-detect-and-normalize\"},{\"Section\": \"ST_DOCUMENT_NORMALIZATION\",\"ImageParameterName\": \"ip-detect-and-normalize\"}]},{\"Name\": \"task-detect-and-normalize-document-gray\",\"ColourMode\": \"ICM_GRAYSCALE\",\"SectionImageParameterArray\": [{\"Section\": \"ST_REGION_PREDETECTION\",\"ImageParameterName\": \"ip-detect-and-normalize\"},{\"Section\": \"ST_DOCUMENT_DETECTION\",\"ImageParameterName\": \"ip-detect-and-normalize\"},{\"Section\": \"ST_DOCUMENT_NORMALIZATION\",\"ImageParameterName\": \"ip-detect-and-normalize\"}]},{\"Name\": \"task-detect-and-normalize-document-color\",\"ColourMode\": \"ICM_COLOUR\",\"SectionImageParameterArray\": [{\"Section\": \"ST_REGION_PREDETECTION\",\"ImageParameterName\": \"ip-detect-and-normalize\"},{\"Section\": \"ST_DOCUMENT_DETECTION\",\"ImageParameterName\": \"ip-detect-and-normalize\"},{\"Section\": \"ST_DOCUMENT_NORMALIZATION\",\"ImageParameterName\": \"ip-detect-and-normalize\"}]},{\"Name\": \"task-detect-document-boundaries\",\"TerminateSetting\": {\"Section\": \"ST_DOCUMENT_DETECTION\"},\"SectionImageParameterArray\": [{\"Section\": \"ST_REGION_PREDETECTION\",\"ImageParameterName\": \"ip-detect\"},{\"Section\": \"ST_DOCUMENT_DETECTION\",\"ImageParameterName\": \"ip-detect\"},{\"Section\": \"ST_DOCUMENT_NORMALIZATION\",\"ImageParameterName\": \"ip-detect\"}]},{\"Name\": \"task-normalize-document-binary\",\"StartSection\": \"ST_DOCUMENT_NORMALIZATION\",\"ColourMode\": \"ICM_BINARY\",\"SectionImageParameterArray\": [{\"Section\": \"ST_REGION_PREDETECTION\",\"ImageParameterName\": \"ip-normalize\"},{\"Section\": \"ST_DOCUMENT_DETECTION\",\"ImageParameterName\": \"ip-normalize\"},{\"Section\": \"ST_DOCUMENT_NORMALIZATION\",\"ImageParameterName\": \"ip-normalize\"}]},{\"Name\": \"task-normalize-document-gray\",\"ColourMode\": \"ICM_GRAYSCALE\",\"StartSection\": \"ST_DOCUMENT_NORMALIZATION\",\"SectionImageParameterArray\": [{\"Section\": \"ST_REGION_PREDETECTION\",\"ImageParameterName\": \"ip-normalize\"},{\"Section\": \"ST_DOCUMENT_DETECTION\",\"ImageParameterName\": \"ip-normalize\"},{\"Section\": \"ST_DOCUMENT_NORMALIZATION\",\"ImageParameterName\": \"ip-normalize\"}]},{\"Name\": \"task-normalize-document-color\",\"ColourMode\": \"ICM_COLOUR\",\"StartSection\": \"ST_DOCUMENT_NORMALIZATION\",\"SectionImageParameterArray\": [{\"Section\": \"ST_REGION_PREDETECTION\",\"ImageParameterName\": \"ip-normalize\"},{\"Section\": \"ST_DOCUMENT_DETECTION\",\"ImageParameterName\": \"ip-normalize\"},{\"Section\": \"ST_DOCUMENT_NORMALIZATION\",\"ImageParameterName\": \"ip-normalize\"}]}],\"ImageParameterOptions\": [{\"Name\": \"ip-detect-and-normalize\",\"BinarizationModes\": [{\"Mode\": \"BM_LOCAL_BLOCK\",\"BlockSizeX\": 0,\"BlockSizeY\": 0,\"EnableFillBinaryVacancy\": 0}],\"TextDetectionMode\": {\"Mode\": \"TTDM_WORD\",\"Direction\": \"HORIZONTAL\",\"Sensitivity\": 7}},{\"Name\": \"ip-detect\",\"BinarizationModes\": [{\"Mode\": \"BM_LOCAL_BLOCK\",\"BlockSizeX\": 0,\"BlockSizeY\": 0,\"EnableFillBinaryVacancy\": 0,\"ThresholdCompensation\": 7}],\"TextDetectionMode\": {\"Mode\": \"TTDM_WORD\",\"Direction\": \"HORIZONTAL\",\"Sensitivity\": 7},\"ScaleDownThreshold\": 512},{\"Name\": \"ip-normalize\",\"BinarizationModes\": [{\"Mode\": \"BM_LOCAL_BLOCK\",\"BlockSizeX\": 0,\"BlockSizeY\": 0,\"EnableFillBinaryVacancy\": 0}],\"TextDetectionMode\": {\"Mode\": \"TTDM_WORD\",\"Direction\": \"HORIZONTAL\",\"Sensitivity\": 7}}]}");
-            } catch (CaptureVisionRouterException e) {
-                throw new RuntimeException(e);
-            }
+            cvr = new CaptureVisionRouter();
         }
         call.resolve();
     }
@@ -48,7 +43,7 @@ public class DocumentNormalizerPlugin extends Plugin {
     @PluginMethod
     public void initLicense(PluginCall call) {
         String license = call.getString("license","DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==");
-        LicenseManager.initLicense(license, getContext(), (isSuccess, error) -> {
+        LicenseManager.initLicense(license, (isSuccess, error) -> {
             if (!isSuccess) {
                 Log.e("DDN", "InitLicense Error: " + error);
                 call.reject(error.getMessage());
@@ -152,7 +147,7 @@ public class DocumentNormalizerPlugin extends Plugin {
         JSObject quad = call.getObject("quad");
         String source = call.getString("source","");
         String path = call.getString("path","");
-        String templateName = call.getString("template","NormalizeDocument_Binary");
+        String templateName = call.getString("template","NormalizeDocument_Default");
         if (path.equals("")) {
             source = source.replaceFirst("data:.*?;base64,","");
         }
@@ -171,14 +166,14 @@ public class DocumentNormalizerPlugin extends Plugin {
                 }else{
                     capturedResult = cvr.capture(path,templateName); //run normalization
                 }
-                NormalizedImageResultItem result = (NormalizedImageResultItem) capturedResult.getItems()[0];
+                DeskewedImageResultItem result = (DeskewedImageResultItem) capturedResult.getItems()[0];
                 JSObject response = new JSObject();
                 JSObject resultObject = new JSObject();
 
                 if (call.getBoolean("saveToFile",false)) {
                     File dir = getContext().getExternalCacheDir();
                     File file = new File(dir,new Date().getTime()+".jpg");
-                    new ImageManager().saveToFile(result.getImageData(),file.getAbsolutePath(),true);
+                    new ImageIO().saveToFile(result.getImageData(),file.getAbsolutePath(),true);
                     resultObject.put("path",file.getAbsolutePath());
                 }
                 if (call.getBoolean("includeBase64",false)) {
@@ -199,7 +194,7 @@ public class DocumentNormalizerPlugin extends Plugin {
     public void detectAndNormalize(PluginCall call) {
         String source = call.getString("source","");
         String path = call.getString("path","");
-        String templateName = call.getString("template","DetectAndNormalizeDocument_Color");
+        String templateName = call.getString("template","DetectAndNormalizeDocument_Default");
         if (path.equals("")) {
             source = source.replaceFirst("data:.*?;base64,","");
         }
@@ -211,13 +206,13 @@ public class DocumentNormalizerPlugin extends Plugin {
                 }else{
                     capturedResult = cvr.capture(path,templateName); //run normalization
                 }
-                NormalizedImageResultItem result = (NormalizedImageResultItem) capturedResult.getItems()[0];
+                DeskewedImageResultItem result = (DeskewedImageResultItem) capturedResult.getItems()[1];
                 JSObject response = new JSObject();
                 JSObject resultObject = new JSObject();
                 if (call.getBoolean("saveToFile",false)) {
                     File dir = getContext().getExternalCacheDir();
                     File file = new File(dir, new Date().getTime()+".jpg");
-                    new ImageManager().saveToFile(result.getImageData(),file.getAbsolutePath(),true);
+                    new ImageIO().saveToFile(result.getImageData(),file.getAbsolutePath(),true);
                     resultObject.put("path",file.getAbsolutePath());
                 }
                 if (call.getBoolean("includeBase64",false)) {
